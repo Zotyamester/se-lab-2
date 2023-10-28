25e7e9dd00595a38802832137523e99c630ac9ac
Zotyamester <szatmary.zoltan1222@gmail.com>
2023-10-28 22:49:55 +0200

    Add more test cases

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -1,6 +1,7 @@
 package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -25,5 +26,32 @@ class TorpedoStoreTest {
     boolean result = store.fire(1);
 
     assertEquals(false, result);
+    assertEquals(false, store.isEmpty());
+    assertEquals(1, store.getTorpedoCount());
+  }
+
+  @Test
+  void fire_Several() {
+    TorpedoStore store = new TorpedoStore(10, 0);
+
+    boolean result1 = store.fire(10);
+
+    assertEquals(true, result1);
+    assertEquals(true, store.isEmpty());
+    assertEquals(0, store.getTorpedoCount());
+  }
+
+  @Test
+  void fire_Zero() {
+    TorpedoStore store = new TorpedoStore(69, 0);
+
+    assertThrows(IllegalArgumentException.class, () -> store.fire(0));
+  }
+
+  @Test
+  void fire_From_Empty_Store() {
+    TorpedoStore store = new TorpedoStore(0, 0);
+
+    assertThrows(IllegalArgumentException.class, () -> store.fire(1));
   }
 }
